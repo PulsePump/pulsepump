9d6741d937cb4d3935216abd22a72ccb9aeaad62
India Henry <157062969+ijh37@users.noreply.github.com>
2026-05-26 17:29:11 +0100

    Update main.py

--- src/pulsepump/firmware/main.py
+++ src/pulsepump/firmware/main.py
@@ -46,13 +46,11 @@ poll.register(sys.stdin, select.POLLIN)
 
 while True:
     now = time.ticks_us()
-
-    v1 = a1.read_u16()
-    sys.stdout.write("{},{},{}\n".format(time.ticks_diff(now, t0), v0, v1, output))
-    next_t = time.ticks_add(next_t, PERIOD_US)
-
     if time.ticks_diff(now, next_t) >= 0:
         v0 = a0.read_u16()  # read current pressure
+        v1 = a1.read_u16()
+        sys.stdout.write("{},{},{},{}\n".format(time.ticks_diff(now, t0), v0, v1, output))
+        next_t = time.ticks_add(next_t, PERIOD_US)
         # Time since last sample
         dt = time.ticks_diff(now, prev_us) / 1_000_000 if prev_us else PERIOD_US / 1_000_000
         # Error in pressure
